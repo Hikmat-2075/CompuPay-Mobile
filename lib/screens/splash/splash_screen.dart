b549a32aa1d558c0d6c20b8a14638c16083baa59
Aryfrmnsyh_ <aryafirmansyah200404@gmail.com>
2026-04-07 11:14:28 +0700

    splash-scree

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:compupay_mobile/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -9,80 +10,85 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    // Delay 3 detik lalu pindah ke LoginScreen
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF8F9FA),
+      backgroundColor: const Color(0xFFF8F9FA),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
             child: Container(
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(50.2),
                 border: Border.all(
-                  color: Color(0xFF6B38D4).withOpacity(0.2),
+                  color: const Color(0xFF6B38D4).withOpacity(0.2),
                   width: 4,
                 ),
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.account_balance_wallet_outlined,
                 size: 100,
                 color: Color(0xFF6B38D4),
               ),
             ),
           ),
-          Center(
-            child: Container(
-              margin: EdgeInsets.only(top: 20),
-              child: RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 48,
-                  ), // Gaya default
-                  children: [
-                    TextSpan(
-                      text: 'Com',
-                      style: TextStyle(fontWeight: FontWeight.w900),
-                    ),
-                    TextSpan(
-                      text: 'p', // Kata yang ingin dibedakan warnanya
-                      style: TextStyle(
-                        color: Color(0xFF6B38D4),
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'u',
-                      style: TextStyle(fontWeight: FontWeight.w900),
-                    ),
-                    TextSpan(
-                      text: 'Pay', // Kata yang ingin dibedakan warnanya
-                      style: TextStyle(
-                        color: Color(0xFF6B38D4),
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                  ],
+
+          const SizedBox(height: 20),
+
+          RichText(
+            text: const TextSpan(
+              style: TextStyle(color: Colors.black, fontSize: 48),
+              children: [
+                TextSpan(
+                  text: 'Com',
+                  style: TextStyle(fontWeight: FontWeight.w900),
                 ),
-              ),
+                TextSpan(
+                  text: 'p',
+                  style: TextStyle(
+                    color: Color(0xFF6B38D4),
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                TextSpan(
+                  text: 'u',
+                  style: TextStyle(fontWeight: FontWeight.w900),
+                ),
+                TextSpan(
+                  text: 'Pay',
+                  style: TextStyle(
+                    color: Color(0xFF6B38D4),
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
             ),
           ),
-          Center(
-            child: Container(
-              margin: EdgeInsets.only(top: 10),
-              child: Text(
-                'FINACIAL CONCIERGE',
-                style: TextStyle(
-                  color: Color(0XFF494454),
-                  fontSize: 14,
-                  letterSpacing: 2.8,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+
+          const SizedBox(height: 10),
+
+          const Text(
+            'FINANCIAL CONCIERGE',
+            style: TextStyle(
+              color: Color(0XFF494454),
+              fontSize: 14,
+              letterSpacing: 2.8,
+              fontWeight: FontWeight.w500,
             ),
           ),
         ],
