1b848a4a58d0d539c16d0e4a22358672d903b624
Hikmat Arif Nugraha <hikmatngrha@gmail.com>
2026-04-14 21:43:38 +0700

    fix attendance screen and login screen

--- lib/screens/attendance/widgets/camera_photo_section.dart
+++ lib/screens/attendance/widgets/camera_photo_section.dart
@@ -1,25 +1,63 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 class CameraPhotoSection extends StatelessWidget {
-  const CameraPhotoSection({super.key});
+  final File? image;
+
+  const CameraPhotoSection({super.key, this.image});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16),
-      height: 220,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: const [
-          BoxShadow(
-            color: Color(0x14000000),
-            blurRadius: 10,
+    return Center(
+      child: Container(
+        width: 280,
+        height: 280,
+        margin: const EdgeInsets.only(top: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xFFF9FAFB),
+          borderRadius: BorderRadius.circular(32),
+          border: Border.all(
+            color: const Color(0xFFE5E7EB),
+            width: 4,
           ),
-        ],
-      ),
-      child: const Center(
-        child: Icon(Icons.camera_alt, size: 50, color: Colors.grey),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(28),
+          child: image == null
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.camera_alt_outlined,
+                      size: 56,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      "No photo yet",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "Take attendance photo",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                )
+              : Image.file(
+                  image!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+        ),
       ),
     );
   }
